22a3a71ef186190069d64f18f6fb782773824ebd
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2015-08-12 16:32:08 +0200

    Remove `crypto` from the required Erlang applications
    This fixes the build of the Mac OS X standalone package. The crypto
    application will be part of the package anyway.

--- package.mk
+++ package.mk
@@ -3,7 +3,7 @@ APP_NAME:=cowlib
 UPSTREAM_GIT:=https://github.com/ninenines/cowlib.git
 UPSTREAM_REVISION:=7d8a571b1e50602d701ca203fbf28036b2cf80f5 # 1.0.1
 RETAIN_ORIGINAL_VERSION:=true
-WRAPPER_PATCHES:=
+WRAPPER_PATCHES:=10-remove-crypto-dependency.patch
 
 ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/$(APP_NAME).app.src
 DO_NOT_GENERATE_APP_FILE=true
